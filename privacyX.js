cef60e66c1ba1b801df06706154af5be2c1912a5
Tan Pinxi <h1110104@nushigh.edu.sg>
2020-01-18 12:51:01 +0800

    Updated UI

--- privacyX.js
+++ privacyX.js
@@ -29,10 +29,7 @@
             xhttp.onreadystatechange = () => {
                 if (this.readyState == 4 && this.status == 200) {
                     //create button to click to start chosen function;
-                    let summary = document.createElement('div');
-                    summary.setAttribute('id', 'summary');
-                    summary.innerHtml = xhttp.responseText;
-                    document.body.appendChild(zNode);
+                    let summary = createPopup(xhttp.responseText);
                 }
             };
             xhttp.open("GET", backend + currentUrl, true);
@@ -47,39 +44,63 @@
             else return "No link to privacy policy page found."
         }
 
+        const createPopup = (content) => {
+            let popup = document.createElement('div');
+            summary.setAttribute('id', 'popup');
+            summary.innerHtml = content;
+            return popup;
+        }
+
+        //creates icon box for button
+        let imgBox = '<img id="iconBox" src="https://i.imgur.com/Ab6y0Ca.png"/>';
+
         //create button to click to start chosen function;
         let zNode = document.createElement('div');
-        zNode.innerHTML = '<button id="myButton" type="button" class="bewton">Click me if you dare!</button>';
+        zNode.innerHTML = '<button id="myButton" type="button" class="bewton">' + imgBox + '</button>';
         zNode.setAttribute('id', 'myContainer');
         document.body.appendChild(zNode);
 
         $('#myButton').on('click', () => {
             let zNode = document.createElement('p');
+            zNode.setAttribute('id','popup');
             zNode.innerHTML = 'Finding privacy policy page...';
-            document.getElementById("myContainer").appendChild(zNode);
+            document.body.appendChild(zNode);
             zNode.innerHTML = findPrivacyLink();
         });
 
         //--- Style our newly added elements using CSS.
         GM_addStyle(`
             #myContainer {
-            position:               fixed;
-            top:                    0;
-            left:                   0;
-            font-size:              20px;
-            background:             orange;
-            border:                 3px outset black;
-            margin:                 5px;
-            opacity:                0.9;
-            z-index:                9999;
-            padding:                5px 20px;
+                position:               fixed;
+                top:                    2px;
+                right:                  2px;
+                background:             white;
+                border:                 0px;
+                margin:                 0px;
+                opacity:                0.9;
+                z-index:                9999;
+                padding:                0px;
             }
             .bewton {
-            cursor:                 pointer;
+                cursor:                 pointer;
             }
             #myContainer p {
-            color:                  red;
-            background:             white;
+                color:                  red;
+                background:             white;
+            }
+            #iconBox {
+                max-width:              16px;
+                max-height:             16px;
+                border:                 0px;
+            }
+            #popup {
+                position:               fixed;
+                top:                    24px;
+                right:                  2px;
+                z-index:                9999;
+                border:                 1px outset black;
+                background:             #f7f7f7;
+                padding:                10px;
             }
         ` );
     });
